61daaa2a1b08c3b222daf3d0b71b2066b501f328
Abel Castro <abelcastrovillar@gmail.com>
2026-04-11 12:56:14 +0100

    feat(mercadona): add product text normalizer

--- mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/model/ProductTextNormalizer.java
+++ mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/model/ProductTextNormalizer.java
@@ -1,4 +1,14 @@
 package org.ulpgc.codestormah.mercadona.model;
 
 public class ProductTextNormalizer {
+    public static String normalizeName(String name) {
+        return  name.toLowerCase()
+                .replace("hacendado", "")
+                .replaceAll("[^\\p{L}0-9 ]", "")
+                .trim();
+    }
+
+    public static String extractBrand(String name) {
+        return name.toLowerCase().contains("hacendado") ? "Hacendado" : "Other";
+    }
 }
