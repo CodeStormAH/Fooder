f8a50ec44635d092f9ba9016c01ea4aa4d246480
Abel Castro <abelcastrovillar@gmail.com>
2026-04-11 20:32:21 +0100

    feat(mercadona): add application entry point

--- mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/Main.java
+++ mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/Main.java
@@ -1,7 +1,18 @@
 package org.ulpgc.codestormah.mercadona;
 
+import org.ulpgc.codestormah.mercadona.controller.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            ProductFeeder feeder = new MercadonaFeeder();
+            ProductSerializer serializer = new DatabaseProductSerializer();
 
+            Controller controller = new Controller(feeder, serializer);
+            controller.execute(-1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
