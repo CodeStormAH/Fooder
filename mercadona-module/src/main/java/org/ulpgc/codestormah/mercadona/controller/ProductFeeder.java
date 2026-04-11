885d8646860f48767f5cf265c553acc7ccd5ec9a
Abel Castro <abelcastrovillar@gmail.com>
2026-04-11 12:51:07 +0100

    feat(mercadona): define feeder and serializer interfaces

--- mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/ProductFeeder.java
+++ mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/ProductFeeder.java
@@ -1,4 +1,9 @@
 package org.ulpgc.codestormah.mercadona.controller;
 
+import org.ulpgc.codestormah.mercadona.model.Product;
+import java.io.IOException;
+import java.util.List;
+
 public interface ProductFeeder {
+    List<Product> getProducts(int limit) throws IOException;
 }
--- mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/ProductSerializer.java
+++ mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/ProductSerializer.java
@@ -1,4 +1,10 @@
 package org.ulpgc.codestormah.mercadona.controller;
 
+import org.ulpgc.codestormah.mercadona.model.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ProductSerializer {
+    void save(List<Product> products) throws SQLException;
 }
