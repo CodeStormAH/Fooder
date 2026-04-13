5768f388e9503591f1d60ed96e64771c6e7c3fc1
Hugo González <hugogzfz@icloud.com>
2026-04-13 12:53:41 +0100

    feat(alcampo): Main class code

--- alcampo-module/src/main/java/org/ulpgc/codestormah/alcampo/Main.java
+++ alcampo-module/src/main/java/org/ulpgc/codestormah/alcampo/Main.java
@@ -1,4 +1,15 @@
 package org.ulpgc.codestormah.alcampo;
 
+import org.ulpgc.codestormah.alcampo.control.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        AlcampoFeeder feeder = new AlcampoScraperFeeder();
+        AlcampoSerializer serializer = new DatabaseAlcampoSerializer();
+
+        AlcampoController controller = new AlcampoController(feeder, serializer);
+
+        controller.execute();
+    }
 }
