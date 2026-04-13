6a6fb4ac2916a3e0dbde5913c13e409fc9ec533e
Hugo González <hugogzfz@icloud.com>
2026-04-13 12:40:30 +0100

    fix(alcampo): delete AlcampoController coments

--- alcampo-module/src/main/java/org/ulpgc/codestormah/alcampo/control/AlcampoController.java
+++ alcampo-module/src/main/java/org/ulpgc/codestormah/alcampo/control/AlcampoController.java
@@ -9,23 +9,19 @@ public class AlcampoController {
     private final AlcampoFeeder feeder;
     private final AlcampoSerializer serializer;
 
-    // Inyección de dependencias a través del constructor
     public AlcampoController(AlcampoFeeder feeder, AlcampoSerializer serializer) {
         this.feeder = feeder;
         this.serializer = serializer;
     }
 
-    // Coordina el flujo general
     public void execute() {
         System.out.println("Iniciando proceso de extracción de datos de Alcampo...");
 
-        // 1. Obtener datos (Extracción y Transformación)
         List<Product> productos = feeder.fetchProducts();
 
         if (productos != null && !productos.isEmpty()) {
             System.out.println("Extracción finalizada. " + productos.size() + " productos obtenidos. Procediendo a guardar...");
 
-            // 2. Persistir datos (Guardado)
             serializer.serialize(productos);
 
             System.out.println("Proceso completado con éxito.");
