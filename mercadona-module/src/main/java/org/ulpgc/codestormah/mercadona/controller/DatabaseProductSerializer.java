87fa6f766a3fba6a32ed0ba782b3b5d11b93bf2a
Abel Castro <abelcastrovillar@gmail.com>
2026-04-13 19:56:57 +0100

    refactor(database): improve product persistence and batch insert

--- mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/DatabaseProductSerializer.java
+++ mercadona-module/src/main/java/org/ulpgc/codestormah/mercadona/controller/DatabaseProductSerializer.java
@@ -40,76 +40,76 @@ public class DatabaseProductSerializer implements ProductSerializer {
 
     @Override
     public void save(List<Product> products) {
-        try (Connection conn = connect()) {
-            initDatabase(conn);
-            persist(products, conn);
-            log(products);
-        } catch (SQLException e) {
-            handleError(e);
+        try (Connection connection = openConnection()) {
+            initializeDatabase(connection);
+            saveProductsAndPrices(products, connection);
+            logSavedProducts(products);
+        } catch (SQLException exception) {
+            handleDatabaseException(exception);
         }
     }
 
-    private Connection connect() throws SQLException {
+    private Connection openConnection() throws SQLException {
         return DriverManager.getConnection(DB_PATH);
     }
 
-    private void initDatabase(Connection conn) throws SQLException {
-        createTables(conn);
+    private void initializeDatabase(Connection connection) throws SQLException {
+        createDatabaseTables(connection);
     }
 
-    private void persist(List<Product> products, Connection conn) throws SQLException {
-        insertProducts(products, conn);
-        insertPrices(products, conn);
+    private void saveProductsAndPrices(List<Product> products, Connection connection) throws SQLException {
+        insertProducts(products, connection);
+        insertPrices(products, connection);
     }
 
-    private void log(List<Product> products) {
+    private void logSavedProducts(List<Product> products) {
         System.out.println("Saved " + products.size() + " products.");
     }
 
-    private void handleError(SQLException e) {
-        e.printStackTrace();
+    private void handleDatabaseException(SQLException exception) {
+        exception.printStackTrace();
     }
 
-    private void createTables(Connection conn) throws SQLException {
-        try (Statement stmt = conn.createStatement()) {
-            stmt.execute(CREATE_PRODUCTS_TABLE_SQL);
-            stmt.execute(CREATE_PRICES_TABLE_SQL);
+    private void createDatabaseTables(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(CREATE_PRODUCTS_TABLE_SQL);
+            statement.execute(CREATE_PRICES_TABLE_SQL);
         }
     }
 
-    private void insertProducts(List<Product> products, Connection conn) throws SQLException {
-        try (PreparedStatement ps = conn.prepareStatement(INSERT_PRODUCTS_SQL)) {
-            for (Product p : products) {
-                fillProduct(ps, p);
-                ps.addBatch();
+    private void insertProducts(List<Product> products, Connection connection) throws SQLException {
+        try (PreparedStatement productInsertStatement = connection.prepareStatement(INSERT_PRODUCTS_SQL)) {
+            for (Product product : products) {
+                fillProduct(productInsertStatement, product);
+                productInsertStatement.addBatch();
             }
-            ps.executeBatch();
+            productInsertStatement.executeBatch();
         }
     }
 
-    private void fillProduct(PreparedStatement ps, Product p) throws SQLException {
-        ps.setString(1, p.id());
-        ps.setString(2, p.name());
-        ps.setString(3, p.normalizedName());
-        ps.setString(4, p.brand());
-        ps.setString(5, p.category());
-        ps.setDouble(6, p.amount());
-        ps.setString(7, p.unit());
+    private void fillProduct(PreparedStatement statement, Product product) throws SQLException {
+        statement.setString(1, product.id());
+        statement.setString(2, product.name());
+        statement.setString(3, product.normalizedName());
+        statement.setString(4, product.brand());
+        statement.setString(5, product.category());
+        statement.setDouble(6, product.amount());
+        statement.setString(7, product.unit());
     }
 
-    private void insertPrices(List<Product> products, Connection conn) throws SQLException {
-        try (PreparedStatement ps = conn.prepareStatement(INSERT_PRICES_SQL)) {
-            for (Product p : products) {
-                fillPrice(ps, p);
-                ps.addBatch();
+    private void insertPrices(List<Product> products, Connection connection) throws SQLException {
+        try (PreparedStatement priceInsertStatement = connection.prepareStatement(INSERT_PRICES_SQL)) {
+            for (Product product : products) {
+                fillPrice(priceInsertStatement, product);
+                priceInsertStatement.addBatch();
             }
-            ps.executeBatch();
+            priceInsertStatement.executeBatch();
         }
     }
 
-    private void fillPrice(PreparedStatement ps, Product p) throws SQLException {
-        ps.setString(1, p.id());
-        ps.setDouble(2, p.unitPrice());
-        ps.setBoolean(3, p.onOffer());
+    private void fillPrice(PreparedStatement statement, Product product) throws SQLException {
+        statement.setString(1, product.id());
+        statement.setDouble(2, product.unitPrice());
+        statement.setBoolean(3, product.onOffer());
     }
 }
