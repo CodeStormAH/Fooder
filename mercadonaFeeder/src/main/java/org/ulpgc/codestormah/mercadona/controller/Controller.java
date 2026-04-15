11c184aad0075547e1691f4142d754f57975fc98
Abel Castro <abelcastrovillar@gmail.com>
2026-04-15 12:49:35 +0100

    refactor(controller): simplify controller using record structure

---
+++ mercadonaFeeder/src/main/java/org/ulpgc/codestormah/mercadona/controller/Controller.java
@@ -0,0 +1,15 @@
+package org.ulpgc.codestormah.mercadona.controller;
+
+import org.ulpgc.codestormah.mercadona.model.Product;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public record Controller(ProductFeeder feeder, ProductSerializer serializer) {
+    public void execute(int maxProducts) throws IOException, SQLException {
+        List<Product> products = feeder.getProducts(maxProducts);
+        System.out.println("Products fetched: " + products.size());
+        serializer.save(products);
+    }
+}
