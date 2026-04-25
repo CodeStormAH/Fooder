3ab79f3f5f73b34ad767f245e3de1aa3f0b4fd57
Hugo González <hugogzfz@icloud.com>
2026-04-25 13:30:03 +0100

    fix(eventStore): main parameters

--- eventStoreBuilder/src/main/java/org/ulpgc/codestormah/eventstore/Main.java
+++ eventStoreBuilder/src/main/java/org/ulpgc/codestormah/eventstore/Main.java
@@ -4,7 +4,7 @@ import org.ulpgc.codestormah.eventstore.control.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        if (args.length < 3) {
+        if (args.length < 4) {
             System.out.println("Usage: <BrokerURL> <Topic> <Source>");
             return;
         }
@@ -12,8 +12,9 @@ public class Main {
         String brokerUrl = args[0]; // tcp://localhost:61616
         String topic = args[1];     // prediction.Product
         String source = args[2];    // Tú: "alcampo" | Compañero: "mercadona"
+        String root = args[3];      // "datalake"
 
-        FileEventStore fileStore = new FileEventStore("datalake");
+        FileEventStore fileStore = new FileEventStore(root);
         ActiveMQSubscriber subscriber = new ActiveMQSubscriber(brokerUrl, topic, source, fileStore);
 
         System.out.println("🚀 Event Store Builder started for source: " + source);
