274589568e2f14202b493fc375e3c7854581005f
Hugo <r6ysfthqbm@privaterelay.appleid.com>
2026-05-08 18:35:22 +0100

    feat(business-unit): add Product class

---
+++ business-unit/src/main/java/org/ulpgc/codestormah/business/model/Product.java
@@ -0,0 +1,31 @@
+package org.ulpgc.codestormah.business.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    private double unitPrice;
+    private String unit;
+    private double amount;
+    private boolean onOffer;
+
+    // Este campo no viene en el 'payload', se lo inyectaremos desde el Evento
+    private transient String source;
+
+    // Constructor vacío necesario para Gson
+    public Product() {}
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getBrand() { return brand; }
+    public String getCategory() { return category; }
+    public double getPrice() { return price; }
+    public double getUnitPrice() { return unitPrice; }
+    public String getUnit() { return unit; }
+    public double getAmount() { return amount; }
+    public boolean isOnOffer() { return onOffer; }
+    public String getSource() { return source; }
+    public void setSource(String source) { this.source = source; }
+}
