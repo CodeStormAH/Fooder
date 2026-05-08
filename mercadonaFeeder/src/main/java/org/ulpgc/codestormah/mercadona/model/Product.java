c303cddfed1087ffbc69cdb9649a74d091821747
Abel <abelcastrovillar@gmail.com>
2026-05-08 19:27:37 +0100

    feat(product): change product names

--- mercadonaFeeder/src/main/java/org/ulpgc/codestormah/mercadona/model/Product.java
+++ mercadonaFeeder/src/main/java/org/ulpgc/codestormah/mercadona/model/Product.java
@@ -1,10 +1,10 @@
 package org.ulpgc.codestormah.mercadona.model;
 
 public record Product(String id, String name, String normalizedName, String brand, String category, double unitPrice,
-                      String unit, double amount, boolean onOffer) {
+                      String unit, double quantity, boolean isOnSale) {
 
     @Override
     public String toString() {
-        return name + " | " + brand + " | " + unitPrice + "€ | " + amount + unit + " | " + category + " | On offer: " + onOffer;
+        return name + " | " + brand + " | " + unitPrice + "€ | " + quantity + unit + " | " + category + " | On offer: " + isOnSale;
     }
 }
