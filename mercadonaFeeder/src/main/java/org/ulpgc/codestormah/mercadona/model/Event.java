bfd746d46b27b12c80bff8a8e196f142a87f54d7
Abel <abelcastrovillar@gmail.com>
2026-05-13 23:34:30 +0100

    feat(model): replace Product payload with flattened Event structure

--- mercadonaFeeder/src/main/java/org/ulpgc/codestormah/mercadona/model/Event.java
+++ mercadonaFeeder/src/main/java/org/ulpgc/codestormah/mercadona/model/Event.java
@@ -5,5 +5,16 @@ import java.time.LocalDateTime;
 public record Event(
         LocalDateTime ts,
         String ss,
-        Product payload
+
+        String id,
+        String name,
+        String normalizedName,
+        String brand,
+        String category,
+
+        double unitPrice,
+        String unit,
+        double quantity,
+
+        boolean isOnSale
 ) {}
